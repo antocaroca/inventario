b4433352cb978d0221a0807f54f067cf043717d7
antonella <antonella_caroca@hotmail.com>
2020-04-17 20:56:00 -0400

    otro commit

--- inventario.py
+++ inventario.py
@@ -20,23 +20,24 @@ class Producto:
         inv.close()
         
 class Inventario(Producto):
-    nombre = input("Ingrese nombre del producto:")
-    cantidad = input("Ingrese cantidad del producto:")
-    precio = input("Ingrese precio del producto:")
-    id_producto = input("Ingrese id numérico del producto:")
-    categoria = input("Ingrese categoría del producto:")
-    peso = input("Ingrese peso del producto:")
+    def ingresar_datos():
+        nombre = input("Ingrese nombre del producto:")
+        cantidad = input("Ingrese cantidad del producto:")
+        precio = input("Ingrese precio del producto:")
+        id_producto = input("Ingrese id numérico del producto:")
+        categoria = input("Ingrese categoría del producto:")
+        peso = input("Ingrese peso del producto:")
 
-    producto1 = Producto(nombre, cantidad, precio, id_producto, categoria, peso)
-    producto1.registrar()
-    print()
-    print("****producto:", producto1.nombre, "se ha registrado correctamente****\n")
+        producto1 = Producto(nombre, cantidad, precio, id_producto, categoria, peso)
+        producto1.registrar()
+        print("\n****producto:", producto1.nombre, "se ha registrado correctamente****")
 
     def buscar_nombre_id_precio():
         archivo = open("inventario.txt", "r")
         nombres = {}
         headers=["Nombre", "Cantidad", "Precio", "ID", "Categoría", "Peso"]
-        nombre_id_precio = input("\n ingrese nombre, id o precio del producto: \n")
+        nombre_id_precio = input("\n ingrese nombre, id o precio del producto: ")
+        print()
 
         for linea in archivo:
             nombre, cantidad, precio, id_numerico, categoria, peso = linea.strip().split(";")
@@ -50,16 +51,14 @@ class Inventario(Producto):
             if precio  not in nombres:
                 nombres[precio] = []
             nombres[precio].append((nombre, cantidad, precio, id_numerico, categoria, peso))
-
-        print()
         print(tabulate(nombres[nombre_id_precio], headers=headers)) 
     
-
     def buscar_categoria():
         archivo = open("inventario.txt", "r")
         categorias = {}
         headers2=["Categoría", "Cantidad", "Precio", "ID", "Nombre", "Peso"] 
-        categoria_a_buscar = input("\nIngrese la categoria del producto: \n")
+        categoria_a_buscar = input("\nIngrese la categoria del producto: ")
+        print()
 
         for linea in archivo:
             nombre, cantidad, precio, id_numerico, categoria, peso = linea.strip().split(";")
@@ -83,25 +82,23 @@ class Inventario(Producto):
             if  cantidad not in cantidades:
                 cantidades[cantidad] = []
             cantidades[cantidad].append((cantidad, categoria, precio, id_numerico, nombre, peso))
-
-        #print(tabulate(cantidades[cantidad], headers=headers3))
-        for  value in cantidades.values():
-            for i in value: 
-                new_i = (i[0])
-                new_i = int(new_i)
-                if new_i < 10:
-                    cant_9[new_i] = []
-                    cant_9[new_i].append((i[0], i[1], i[2], i[3], i[4], i[5]))
-
-                    resultado = (cant_9[new_i])
-                    print()    
-                    print(tabulate(resultado, headers=headers3))
-                    print()
-                       
-        archivo.close()
         
+        lista_de_productos = []
+        for key, value in cantidades.items():
+            lista_de_productos.append((list(value)))
+        
+        lista_prod_final = []
+        for i in lista_de_productos:
+            for j in i:
+                if (int(j[0])) < 10:
+                    lista_prod_final.append(j)
+        print("***** Lista de productos bajo 10 unidades *****\n")
+        print(tabulate(lista_prod_final, headers=headers3))
+        archivo.close()
+
+
+Inventario.ingresar_datos()              
 Inventario.buscar_nombre_id_precio()
-print()
 Inventario.buscar_categoria()
 print()
 Inventario.buscar_top_9()
